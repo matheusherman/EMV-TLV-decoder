bf6d87c637fdcf0ae93ea2d4ec485cacfcb093b7
Matheus Herman <matheushbandrade@hotmail.com>
2026-04-13 20:17:48 -0300

    feat: add 9F40 tag definition

--- app/src/main/java/com/example/testepositivo/repository/TagRepository.java
+++ app/src/main/java/com/example/testepositivo/repository/TagRepository.java
@@ -16,6 +16,9 @@ public class TagRepository {
 //        else if (tag.equals("9F34")) {
 //            return create9F34();
 //        }
+        else if (tag.equals("9F40")) {
+            return create9F40();
+        }
         return null;
     }
 
@@ -57,6 +60,76 @@ public class TagRepository {
         return new TagDefinition(
                 "9F33",
                 Arrays.asList(byte1, byte2, byte3)
+        );
+    }
+
+    private static TagDefinition create9F40() {
+
+        // Byte 1 - Transaction Type Capability
+        ByteDefinition byte1 = new ByteDefinition(
+                1,
+                Arrays.asList(
+                        new BitDefinition(8, "Cash"),
+                        new BitDefinition(7, "Goods"),
+                        new BitDefinition(6, "Services"),
+                        new BitDefinition(5, "Cashback"),
+                        new BitDefinition(4, "Inquiry"),
+                        new BitDefinition(3, "Transfer"),
+                        new BitDefinition(2, "Payment"),
+                        new BitDefinition(1, "Administrative")
+                )
+        );
+
+        // Byte 2 - Transaction Type Capability
+        ByteDefinition byte2 = new ByteDefinition(
+                2,
+                Arrays.asList(
+                        new BitDefinition(8, "Cash Deposit")
+                )
+        );
+
+        // Byte 3 - Terminal Data Input Capability
+        ByteDefinition byte3 = new ByteDefinition(
+                3,
+                Arrays.asList(
+                        new BitDefinition(8, "Numeric keys"),
+                        new BitDefinition(7, "Alphabetic and special character keys"),
+                        new BitDefinition(6, "Command keys"),
+                        new BitDefinition(5, "Function keys")
+                )
+        );
+
+        // Byte 4 - Terminal Data Output Capability
+        ByteDefinition byte4 = new ByteDefinition(
+                4,
+                Arrays.asList(
+                        new BitDefinition(8, "Print, attendant"),
+                        new BitDefinition(7, "Print, cardholder"),
+                        new BitDefinition(6, "Display, attendant"),
+                        new BitDefinition(5, "Display, cardholder"),
+                        new BitDefinition(2, "Code table 10"),
+                        new BitDefinition(1, "Code table 9")
+                )
+        );
+
+        // Byte 5 - Terminal Data Output Capability (Code tables)
+        ByteDefinition byte5 = new ByteDefinition(
+                5,
+                Arrays.asList(
+                        new BitDefinition(8, "Code table 8"),
+                        new BitDefinition(7, "Code table 7"),
+                        new BitDefinition(6, "Code table 6"),
+                        new BitDefinition(5, "Code table 5"),
+                        new BitDefinition(4, "Code table 4"),
+                        new BitDefinition(3, "Code table 3"),
+                        new BitDefinition(2, "Code table 2"),
+                        new BitDefinition(1, "Code table 1")
+                )
+        );
+
+        return new TagDefinition(
+                "9F40",
+                Arrays.asList(byte1, byte2, byte3, byte4, byte5)
         );
     }
 }
